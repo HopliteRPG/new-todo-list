27941233a2b4e4bec600e363019088030b413aed
HopliteRPG <146594930+HopliteRPG@users.noreply.github.com>
2025-01-21 17:33:48 -0500

    i can now get the index of the object with the id that pressing the delete button returned

--- src/event-listener-code/delete-project.js
+++ src/event-listener-code/delete-project.js
@@ -1,13 +1,29 @@
 export{getProjectHTMLId}
+import { projectDisplayArray } from "..";
+
+
 function getProjectHTMLId(dom,targetHTMLClassname){
     let targetHTML = dom.querySelectorAll(targetHTMLClassname);
     targetHTML.forEach(deleteButton => {
         deleteButton.addEventListener("click",event=>{
-            let deleteButtonParentDivId = event.currentTarget.parentNode;
-            console.log(deleteButtonParentDivId)
+
+            let deleteButtonParentDivId = event.currentTarget.parentNode.id;
+            
+            function getUniqueId(object) { 
+                return object.id == deleteButtonParentDivId;
+              }
+            
+            const selectedIndex = projectDisplayArray.findIndex(getUniqueId)
+            alert("Selected Index: " + selectedIndex)
+
+
+
+
         });
     });
-
-
-
 }
+
+
+
+
+
